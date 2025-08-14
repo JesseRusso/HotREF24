00a02cfbb2a12bbb9fa41ca70a9e29923f65148c
Jesse <jesserusso90@gmail.com>
2025-08-14 14:23:11 -0700

    Target OS change

--- HotPort/MainWindow.xaml.cs
+++ HotPort/MainWindow.xaml.cs
@@ -10,7 +10,7 @@ namespace HotPort
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
